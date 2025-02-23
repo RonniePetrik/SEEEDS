4197d4796fe8d7f09fb5bc6f0c9f7773d9c6916f
RonniePetrik <ronniepetrik2@gmail.com>
2025-02-23 08:19:25 -0500

    Added Battleship Game

--- SEEEDS.py
+++ SEEEDS.py
@@ -32,19 +32,19 @@ print('                                                                         
 time.sleep(1)
 print('                                                                                   |                                                           |')
 time.sleep(1)
-print('                                                                                   |                 version 0.2.0.0.7                         |')
+print('                                                                                   |                 version 4.8.9.2.8                         |')
 time.sleep(1)
 print('                                                                                   |                                                           |')
 time.sleep(1)
 print('                                                                                   |             Original Release:12/31/2024                   |')
 time.sleep(1)
-print('                                                                                   |            Last Major Update:2/18/2025                    |')
+print('                                                                                   |            Last Major Update:2/23/2025                    |')
 time.sleep(1)
 print('                                                                                   |           Read User Manual (type \'help\')                  |')
 time.sleep(1)
 print('                                                                                   |                                                           |')
 time.sleep(1)
-print('                                                                                   |       C input will not work until games are added         |')
+print('                                                                                   |                                                           |')
 time.sleep(1)
 print('                                                                                   |                                                           |')
 time.sleep(1)
@@ -96,12 +96,126 @@ while True:
         input_mode_c = input('C>>>>')                   #Display this
         if input_mode_c == 'simplegame_coincollector':
             coin_collector()
-        elif input_mode_c == 'simplegame_shootthefruit':  #if you enter the command for shoot the fruit then pass (game has not been added yet)
-            pass
+        elif input_mode_c == 'simplegame_battleship':  #if you enter the command for battleship
+            import random
+
+            def createnewBoard():                                          #Start Battleship Game
+                BombBoard = ['~','~','~','~','~','~','~','~','~']
+                alreadytaken=''
+                position=random.randint(0,8)
+                bombs=1
+                while bombs <=3:
+                    while str(position) not in alreadytaken:
+                        BombBoard[position]='X'
+                        alreadytaken=alreadytaken + str(position)
+                        bombs = bombs + 1
+                    else:
+                        position=random.randint(0,8)
+                   
+                return BombBoard
+                
+            def displayBoard(BombBoard):
+                print('  1' + ' 2' + ' 3')
+                print('A', end='')
+                for i in range(3):
+                    print(' ' + BombBoard[i], end= '')
+                print()
+            
+                print('B', end='')
+                for i in range(3):
+                    print(' ' + BombBoard[i+3], end= '')
+                print()
+            
+                print('C', end='')
+                for i in range(3):
+                    print(' ' + BombBoard[i+6], end= '')
+                print()
+            
+            def checkHit(row,column, gameBoard):
+                Hit=False
+                if row== 'A' or row == 'a':
+                    if gameBoard[int(column)-1] == 'X':
+                        Hit=True
+                        
+                if row== 'B' or row == 'b':
+                    if gameBoard[int(column)+2] == 'X':
+                        Hit=True
+                        
+                if row== 'C' or row == 'c':
+                    if gameBoard[int(column)+5] == 'X':
+                        Hit=True
+                
+                return Hit
+            
+            def checkForIndex(row,column,gameboard):
+                if row== 'A' or row == 'a':
+                    if gameBoard[int(column)-1] == 'X':
+                        index=int(column)-1
+                        
+                if row== 'B' or row == 'b':
+                    if gameBoard[int(column)+2] == 'X':
+                        index=int(column)+2
+                        
+                if row== 'C' or row == 'c':
+                    if gameBoard[int(column)+5] == 'X':
+                        index=int(column)+5
+                
+                return index
+                
+            #gamebegins   
+            playAgain=True
+            
+            while playAgain==True:
+                print('B A T T L E - S H I P S\n' + '=======================')
+                print('There are 3 Ships in the Ocean,\n' + 'you have 5 guesses to bomb them all! Good Luck!')
+                print()
+                playerBoard = ['?','?','?','?','?','?','?','?','?']
+                gusses=0
+                hits=0
+                gameBoard=createnewBoard()
+                while gusses<=4:
+                    displayBoard(playerBoard)
+                    print()
+                    print('Choose a row (A/B/C):')
+                    row=input()
+                    while row != 'a' and row != 'A' and row != 'b' and row != 'B' and row != 'c' and row != 'C':
+                        print('You can only choose between A/B/C!')
+                        row=input()
+                    
+                    print('Choose a column (1/2/3):')
+                    column=input()
+                    while column != '1' and column != '2' and column != '3':
+                        print('You can only choose between 1/2/3!')
+                        column=input()
+                        
+                    Hit=checkHit(row,column,gameBoard)
+                    if Hit==True:
+                        print('You Bombed a ship!')
+                        hits = hits + 1
+                        if hits==3:
+                            print('You Did It!, would you like to play again? (y/n):')
+                            playerinput=input()
+                            if playerinput == 'y' or playerinput == 'Y':
+                                playAgain=True
+                            else:
+                                playAgain=False
+                                break
+                        playerBoard[checkForIndex(row,column,gameBoard)] = 'X'
+                        gusses = gusses + 1
+                    else:
+                        print('You Missed!')
+                        gusses = gusses + 1
+                else:
+                    print('You ran out of guesses!, would you like to play again? (y/n):')
+                    playerinput=input()
+                    if playerinput == 'y' or playerinput == 'Y':
+                        playAgain=True
+                    else:
+                        break
         else:
             print('Error, No such command') #If you type in a nonexistent input mode, display this.
     elif input_mode == 'help':
-        input_help = input('What Do You Need Help With? commands or manual?>>')
+        input_help = input('What Do You Need Help With? commands or manual? Type one!>>')
         if input_help == ('commands'):
             print('Commands: in input A (type and uppercase A into MM>): random_numb=random number 1 to 100, random_numb_2=random number 1 to 1000, note=type a note, note_2=type a second note, show_note=show first note, show_note_2=show second note, (In MM>) passpick=open Password Picker 25')
         elif input_help == ('manual'):
@@ -123,9 +237,9 @@ while True:
         print('')
         print('')
         print('\n')
-        print('The 3rd version of Password Picker')
-        print('last upadated: 0/0/0     last major update: 12/30/2024')
-        print('\n\n\n\n\n')
+        print('The 3rd Generation of Password Picker')
+        print('last upadated: 2/23/2025     last major update: 12/30/2024')
+        print('\n\n\n\n')
         while True:
             adjective = random.choice(adjectives)
             noun = random.choice(nouns)
@@ -143,6 +257,6 @@ while True:
                 break  #breaks the loop
             
     else:
-        print('No such input!')    #Tells you that the input you have put into the main menu does not exist
+        print('No such input!, Try A, B, or C!')    #Tells you that the input you have put into the main menu does not exist
                 
         
